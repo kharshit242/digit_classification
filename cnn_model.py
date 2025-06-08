a990a974c48201d0695956008112334ffa86caf2
Harshit Kumar <kharshit242@gmail.com>
2025-06-08 15:28:06 +0530

    error fixed mnist

--- cnn_model.py
+++ cnn_model.py
@@ -1,32 +1,29 @@
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.utils import to_categorical
 
-# Load data
+# Load and preprocess data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# Preprocessing
 x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype("float32") / 255.0
 y_train = to_categorical(y_train, 10)
 y_test = to_categorical(y_test, 10)
 
-# Build CNN
-model = Sequential([
-    Conv2D(32, kernel_size=3, activation='relu', input_shape=(28,28,1)),
-    MaxPooling2D(pool_size=2),
-    Conv2D(64, kernel_size=3, activation='relu'),
-    MaxPooling2D(pool_size=2),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dense(10, activation='softmax')
-])
+# Functional API
+inputs = Input(shape=(28, 28, 1))
+x = Conv2D(32, kernel_size=3, activation="relu")(inputs)
+x = MaxPooling2D(pool_size=2)(x)
+x = Conv2D(64, kernel_size=3, activation="relu")(x)
+x = MaxPooling2D(pool_size=2)(x)
+x = Flatten()(x)
+x = Dense(128, activation="relu")(x)
+outputs = Dense(10, activation="softmax")(x)
 
-# Compile and train
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5)
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
 
-# Save the model
+# Save model
 model.save("mnist_cnn_model.h5")
